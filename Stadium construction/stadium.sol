96f7803288cda8cce29eb65557cc4e57c821a0c9
AKHIL SINGH CHAUHAN <60491139+akhilub@users.noreply.github.com>
2021-01-17 06:48:07 -0500

    Add files via upload

---
+++ Stadium construction/stadium.sol
@@ -0,0 +1,22 @@
+# Solution for model stadium
+# Objective value = 64
+Z1 64
+S_1 0
+S_2 2
+S_3 18
+S_4 18
+S_5 27
+S_6 37
+S_7 26
+S_8 43
+S_9 43
+S_10 26
+S_11 43
+S_12 52
+S_13 28
+S_14 18
+S_15 26
+S_16 46
+S_17 54
+S_18 63
+S_End 64
